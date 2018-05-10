fb8b2ac9865f2cf15c42d177d3e8ae9b92d3f526
anderson <anderson@java-devops>
2018-05-10 10:45:14 -0400

    Started separating files to build MVC architecture
    Separated MenuSearchServlet.java from searchResults.jsp to hold
    controller and view respectively while MenuDao.java still handles the
    model of the application

--- Restaurant/src/main/java/com/virtualpairprogrammers/servlets/MenuSearchServlet.java
+++ Restaurant/src/main/java/com/virtualpairprogrammers/servlets/MenuSearchServlet.java
@@ -1,9 +1,11 @@
 package com.virtualpairprogrammers.servlets;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -18,30 +20,16 @@ public class MenuSearchServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 5742658646295132901L;
 
-	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		response.setContentType("text/html");
+	public void service(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 		
 		String searchTerm = request.getParameter("searchTerm");
-		
 		MenuDao menuDao = MenuDaoFactory.getMenuDao();
 		List<MenuItem> menuItems = menuDao.find(searchTerm);
 		
-		if (menuItems.size() > 0) {
-		
-			out.println("<html><body><h1>Ricky's Restaurant</h1>");
-			out.println("<h2>Dishes containing " + searchTerm + "</h2><ul>");
-			for (MenuItem menuItem : menuItems) {
-				out.println("<li>" + menuItem + " " + menuItem.getDescription() + "</li>");
-			}
-			
-			out.println("</ul></body></html>");
-		}
-		else {
-			out.println("<html><body><h1>Ricky's Restaurant</h1>");
-			out.println("<p>I'm sorry, there are no dishes containing " + searchTerm);
-			out.println("</p></body></html>");
-		}
-		out.close();
+		request.setAttribute("menuItems", menuItems);
+
+		ServletContext context = getServletContext();
+		RequestDispatcher dispatch = context.getRequestDispatcher("/searchResults.jsp");
+		dispatch.forward(request, response);
 	}
 }

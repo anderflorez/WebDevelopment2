859839feeb009f07f48be5f477303b4057c58c85
anderson <anderson@java-devops>
2018-05-14 09:34:51 -0400

    Modified the order display for the kitchen
    Order list will now contain all orders available or not completed on
    /KitchenManager.html
    
    Both order screen and order update screen will be on the same page

--- Restaurant/src/main/java/com/virtualpairprogrammers/websockets/KitchenDisplaySessionHandler.java
+++ Restaurant/src/main/java/com/virtualpairprogrammers/websockets/KitchenDisplaySessionHandler.java
@@ -9,6 +9,8 @@ import javax.websocket.Session;
 
 import org.json.JSONObject;
 
+import com.virtualpairprogrammers.data.MenuDao;
+import com.virtualpairprogrammers.data.MenuDaoFactory;
 import com.virtualpairprogrammers.domain.Order;
 
 public class KitchenDisplaySessionHandler {
@@ -18,6 +20,7 @@ public class KitchenDisplaySessionHandler {
 	public void addSession(Session session) {
 		
 		sessions.add(session);
+		sendAllOrders(session);
 	}
 
 	public void removeSession(Session session) {
@@ -36,7 +39,16 @@ public class KitchenDisplaySessionHandler {
 		}
 	}
 	
-	public void newOrder(Order order) {
+	private void sendMessage(JSONObject message, Session session) {
+		
+		try {
+			session.getBasicRemote().sendText(message.toString());
+		} catch (IOException e) {
+			removeSession(session);
+		}
+	}
+	
+	public JSONObject generateJSONForOrder(Order order) {
 		
 		JSONObject json = new JSONObject();
 		json.append("id", order.getId().toString());
@@ -44,7 +56,12 @@ public class KitchenDisplaySessionHandler {
 		json.append("content", order.toString());
 		json.append("action", "add");
 		json.append("update", new Date().toString());
-		sendMessage(json);
+		return json;
+	}
+	
+	public void newOrder(Order order) {
+		
+		sendMessage(generateJSONForOrder(order));
 	}
 	
 	public void ammendOrder(Order order) {
@@ -53,6 +70,16 @@ public class KitchenDisplaySessionHandler {
 		json.append("id", order.getId().toString());
 		json.append("action", "remove");
 		sendMessage(json);
+		if (!order.getStatus().equals("ready for collection"))
 		newOrder(order);
+	}
+	
+	public void sendAllOrders(Session session) {
+		MenuDao dao = MenuDaoFactory.getMenuDao();
+		List<Order> orders = dao.getAllOrders();
+		for (Order order : orders) {
+			if (!order.getStatus().equals("ready for collection"))
+			sendMessage(generateJSONForOrder(order), session);
+		}
 	}
 }
--- Restaurant/src/main/java/com/virtualpairprogrammers/websockets/KitchenDisplayWebsocket.java
+++ Restaurant/src/main/java/com/virtualpairprogrammers/websockets/KitchenDisplayWebsocket.java
@@ -2,9 +2,16 @@ package com.virtualpairprogrammers.websockets;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+
+import org.json.JSONObject;
+
+import com.virtualpairprogrammers.data.MenuDao;
+import com.virtualpairprogrammers.data.MenuDaoFactory;
+import com.virtualpairprogrammers.domain.Order;
 
 @ServerEndpoint("/kitchenManagement")
 public class KitchenDisplayWebsocket {
@@ -27,6 +34,20 @@ public class KitchenDisplayWebsocket {
 	public void onError(Throwable error) {
 		
 		throw new RuntimeException(error);
+	}
+	
+	@OnMessage
+	public void handleMessage(String message, Session session) {
+		JSONObject json = new JSONObject(message);
+		Long id = json.getLong("id");
+		String status = json.getString("status");
+		
+		MenuDao menuDao = MenuDaoFactory.getMenuDao();
+		menuDao.updateOrderStatus(id,status);
+		
+		Order order = menuDao.getOrder(id);
+		KitchenDisplaySessionHandler handler = KitchenDisplaySessionHandlerFactory.getHandler();
+		handler.ammendOrder(order);
 	}
 
 }
